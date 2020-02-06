e1f155440439c730d02e4f6a0500410c1ee71774
datsu0 <atsu34412@gmail.com>
2020-02-06 21:36:43 +0900

    first commit

---
+++ 153/a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <map>
+#include <string>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+
+    int i=0;
+    int H,A;
+    cin >> H >> A;
+    while(H>0){
+        H=H-A;
+        i++;
+    }
+    cout << i << endl;
+    return 0;
+}
